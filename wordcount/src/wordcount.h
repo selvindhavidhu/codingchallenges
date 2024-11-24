e90fb38c709f7f65f49f4662d54a9bdc96d583d4
Selvin Dhavidhu <selvin.dhavidhu@gmail.com>
2024-11-24 12:34:36 +0530

    Set up the initial project files

---
+++ wordcount/src/wordcount.h
@@ -0,0 +1,9 @@
+﻿#ifndef CODINGCHALLENGES_WORDCOUNT_WORDCOUNT_H_
+#define CODINGCHALLENGES_WORDCOUNT_WORDCOUNT_H_
+
+#include <algorithm>
+#include <iostream>
+#include <string>
+#include <vector>
+
+#endif
